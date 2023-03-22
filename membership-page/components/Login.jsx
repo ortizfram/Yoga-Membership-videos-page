d366e7003890f2e14dbb4660f1f2c5fa1a22b10b
ortizfram <ortizfranco48@gmail.com>
2023-03-22 11:01:48 -0300

    button register

--- membership-page/components/Login.jsx
+++ membership-page/components/Login.jsx
@@ -1,15 +1,17 @@
 import React from "react";
+import { FcGoogle } from "react-icons/fc";
 
 const Login = () => {
   return (
     <div className="grid grid-cols-2 bg-gradient-to-r from-purple-600 via-pink-600 to-blue-600 drop-shadow-2xl">
       <div className="bg-login-image bg-cover bg-center h-screen grid place-items-center"></div>
       <div className="flex items-center justify-center flex-col m-auto p-2">
-        <h3 className="text-3xl font-bold text-white">Se miembro</h3>
+        <h3 className="text-6xl font-bold text-white">Se miembro</h3>
         <p className="text-white">comienza con tu experiencia.</p>
-        <button className="drop-shadow-2xl mt-4 px-6 py-4 rounded-full bg-white hover:bg-blue-600 hover:text-white text-black">
+        <div className="flex gap-4 cursor-pointer drop-shadow-2xl mt-4 px-6 p-4 rounded-[6px] bg-white  text-black">
+          <FcGoogle className="text-[30px]" />
           Registrate
-        </button>
+        </div>
       </div>
     </div>
   );
