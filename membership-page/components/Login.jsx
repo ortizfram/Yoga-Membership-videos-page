af9d8564199c18f61565c6bf4d90611336f89a2a
ortizfram <ortizfranco48@gmail.com>
2023-03-22 11:27:21 -0300

    google auth login screen

--- membership-page/components/Login.jsx
+++ membership-page/components/Login.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { FcGoogle } from "react-icons/fc";
-
+import { signIn } from "next-auth/react";
 const Login = () => {
   return (
     <div className="grid grid-cols-2 bg-gradient-to-r from-purple-600 via-pink-600 to-blue-600 drop-shadow-2xl">
       <div className="bg-login-image bg-cover bg-center h-screen grid place-items-center"></div>
-      <div className="flex items-center justify-center flex-col m-auto p-2">
-        <h3 className="text-6xl font-bold text-white">Se miembro</h3>
+      <div
+        onClick={() => signIn("google")}
+        className="flex items-center justify-center flex-col m-auto p-2"
+      >
+        <h3 className="text-6xl font-bold text-white">Sé miembro</h3>
         <p className="text-white">comienza con tu experiencia.</p>
         <div className="flex gap-4 cursor-pointer drop-shadow-2xl mt-4 px-6 p-4 rounded-[6px] bg-white  text-black">
           <FcGoogle className="text-[30px]" />
