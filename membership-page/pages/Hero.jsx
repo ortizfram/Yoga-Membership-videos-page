e0b75a85aecef9c0ae3c238face02090b8ae2fcd
ortizfram <ortizfranco48@gmail.com>
2023-04-25 10:53:24 -0300

    respoonsiveness slider hero

--- membership-page/pages/Hero.jsx
+++ membership-page/pages/Hero.jsx
@@ -14,27 +14,22 @@ const Hero = () => {
   return (
     <div
       id="hero"
-      className="grid 2xl:grid-cols-2 mx-auto grid-cols-1 bg-hero-image w-full h-screen bg-cover bg-center px-4"
+      className="mx-auto grid grid-cols-1 2xl:grid-cols-2  bg-hero-image w-full h-screen bg-cover bg-center  p-[10vw]"
     >
       {/* WELCOME */}
-      <div className="hidden md:flex-row md:gap-x-[100px]">
-        {/* ___message */}
+      {/* <div className="hidden md:flex-row md:gap-x-[50px]">
+        ___message
         <div className="justify-center text-white/40">
           <h3 className="text-xl font-thin text-white/75">{user.firstName}</h3>
           <p>Bienvenido/a a...</p>
         </div>
+      </div> */}
+
+      <div className="grid md:flex items-center justify-center">
+        <Image src="/white-logo-buonavibra.png" width={600} height={600} />
       </div>
 
-      <div>
-        <Image
-          src="/white-logo-buonavibra.png"
-          width={2400}
-          height={2400}
-          className=""
-        />
-      </div>
-
-      <Slider />
+      <Slider className="grid" />
     </div>
   );
 };
